75316aff6644cd4d1a9aefac27280a1f05119ff2
Ethan Itovitch <ethan.itovitch@mail.mcgill.ca>
2025-10-21 19:31:27 -0300

    linted

--- backend/src/queues/index.ts
+++ backend/src/queues/index.ts
@@ -1,6 +1,6 @@
-import { Queue } from "bullmq";
-import { ExampleEvent, QueueName } from "@/types/queues";
-import { config } from "@/config";
+import { Queue } from 'bullmq'
+import { ExampleEvent, QueueName } from '@/types/queues'
+import { config } from '@/config'
 
 export const exampleQueue = new Queue<ExampleEvent>(QueueName.EXAMPLE, {
   connection: {
@@ -11,14 +11,18 @@ export const exampleQueue = new Queue<ExampleEvent>(QueueName.EXAMPLE, {
       },
     }),
   },
-});
+})
 
-export const addExampleEvent = async (event: ExampleEvent, attempts: number = 3, backoff: number = 1000) => {
+export const addExampleEvent = async (
+  event: ExampleEvent,
+  attempts: number = 3,
+  backoff: number = 1000,
+) => {
   await exampleQueue.add(event.type, event, {
     attempts,
     backoff: {
       type: 'exponential',
       delay: backoff,
     },
-  });
+  })
 }
